e70ae36e247d08a289abeb55b01f2390577c3f78
mdeandwibekti <mdeandwibekti@gmail.com>
2024-03-07 20:20:43 +0700

    bubblesortarray

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -24,3 +24,23 @@ void input() {         //prosedur untuk input
 		cin >> arr[i];
 	}
 }
+void bubbleshortarray() { // prosedur untuk mengurutkan array dengan metode bubble short
+	int pass = 1; // step 1
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) {            //step 3
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;      //step 4
+
+		cout << "\npass " << pass - 1 << ": "; // output ke layar
+		for (int k = 0; k < n; k++) {        // looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";           //output ke layar
+
+		}
+		cout << endl;
+	} while (pass <= n - 1);   //step 5
+}
