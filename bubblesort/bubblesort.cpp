ac22663226d7c9302a57875230b7486e7a38472d
mdeandwibekti <mdeandwibekti@gmail.com>
2024-03-07 20:16:54 +0700

    variable global

---
+++ bubblesort/bubblesort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];           //deklarasi array a dengan ukuran 20
+int n;                 //deklarasi variabel n untuk menyimpan banyaknya elemen
+
